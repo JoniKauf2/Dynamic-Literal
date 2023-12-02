a56d0bbb228c97279d9a2d0e733d8d94ad9ee966
JoniKauf2 <93454391+JoniKauf2@users.noreply.github.com>
2023-12-02 23:35:07 +0100

    Update dynamic_literal.py

--- dynamic_literal.py
+++ dynamic_literal.py
@@ -2,20 +2,17 @@ import uuid
 from typing import Literal, Callable
 from enum import Enum
 
-def literalize(*values: str, anti_interning_method: Callable = lambda: str(uuid.uuid4()), **method_args) -> Literal['*values']:
+def literalize(*values: str) -> Literal['*values']:
     """
     Creates a new Literal with the given values.\n
-    This only changes the runtime type of the the Literal, while
-    the static type is based off the `anti_interning_method` and
-    cannot change. Therefore it has very limited use and is not designed
-    for general purpose use.\n
+    This only changes the runtime type of the the Literal.
     This was designed for libraries, more specifically for discord.py,
     that use type hinting to type check arguments. This allows the 
     programmer to define constants easily and turn them into a Literal
     for easy type hinting and more maintainability.
     """
     class LiteralElem(Enum):
-        ELEM = anti_interning_method(**method_args)
+        ELEM = values
     
     literal = Literal[LiteralElem.ELEM]
     literal.__args__ = values
